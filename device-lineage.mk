998611a1764bb4a1b8110607b39dc9f60bf0e04a
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    shusky: device-lineage: Set TARGET_PREBUILT_KERNEL
    Change-Id: I156a4af77bd619f855f734983b24677531156c4d
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,3 +4,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/shusky/overlay-lineage
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/shusky-kernel/Image.lz4
