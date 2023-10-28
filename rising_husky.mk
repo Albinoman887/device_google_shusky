ce4abf05933c6deaec3869ac2655539b21bbba27
Matt Filetto <matt.filetto@gmail.com>
2023-10-27 22:37:00 -0700

    add rising specifics
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- rising_husky.mk
+++ rising_husky.mk
@@ -25,11 +25,35 @@ $(call inherit-product, device/google/shusky/device-husky.mk)
 # Inherit risingOS vendor stuffs
 $(call inherit-product, vendor/rising/config/rising.mk)
 
-PRODUCT_NAME := aosp_husky
+# Device Identifiers
+PRODUCT_NAME := rising_husky
 PRODUCT_DEVICE := husky
-PRODUCT_MODEL := AOSP on husky
-PRODUCT_BRAND := Android
+PRODUCT_MODEL := Pixel 8 Pro
+PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
+RISING_MAINTAINER := Albinoman887
+RISING_CHIPSET := Tensor
+RISING_STORAGE := 128gb
+RISING_RAM := 12gb
+RISING_BATTERY := 5000mah
+RISING_DISPLAY := 3200×1440
+
+# GMS
+WITH_GMS := true
+TARGET_PREBUILT_PIXEL_LAUNCHER := true
+# Rising testing flag for full p2023 experience
+VENDOR_TEST_ENABLE_PIXEL_EXPERIENCE := true
+# Ship pixel features (adaptivecharging, dreamliner etc)
+TARGET_ENABLE_PIXEL_FEATURES := true
+# Use google telephony framework
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_PREBUILT_SOUND_MODEL := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=husky \
+    PRIVATE_BUILD_DESC="husky-user 14 UD1A.230803.022.B1 10726908 release-keys"
+
+BUILD_FINGERPRINT := google/husky/husky:14/UD1A.230803.022.B1/10726908:user/release-keys
 
 DEVICE_MANIFEST_FILE := \
 	device/google/shusky/manifest.xml
